725e389076394f434324c58d5dea648bf0ced95b
isharrma <54413011+isharrma@users.noreply.github.com>
2020-09-19 16:19:52 +0530

    Integration
    Integrated color picker with webcam

--- color.py
+++ color.py
@@ -29,11 +29,12 @@ while True:
     s_max = cv2.getTrackbarPos("Sat Max","Trackbars")
     v_min = cv2.getTrackbarPos("Val Min","Trackbars")
     v_max = cv2.getTrackbarPos("Val Max","Trackbars")
-    print(h_min , h_max , s_min, s_max, v_min, v_max)
+    print(h_min , s_min , v_min, h_max, s_max, v_max)
     lower = np.array([h_min, s_min, v_min])
     upper = np.array([h_max, s_max, v_max])
     mask = cv2.inRange(imgHSV,lower,upper)
     imgResult = cv2.bitwise_and(img,img,mask=mask)
+
     cv2.imshow("webcam" ,img)
     cv2.imshow("HSV", imgHSV)
     cv2.imshow("Mask", mask)
--- maina_app.py
+++ maina_app.py
@@ -1,6 +1,17 @@
 import cv2
 import numpy as np
 
+myColors = [[0 ,82 ,165 ,179, 147 ,204],
+            [0 ,179, 132, 212, 174, 231],
+            ]
+
+def findColor(img,myColors):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower = np.array(myColors[0][0:3])
+    upper = np.array(myColors[0][3:6])
+    mask = cv2.inRange(imgHSV,lower,upper)
+    cv2.imshow("img",mask)            
+
 frame_height = 640
 frame_width = 480
 cap = cv2.VideoCapture(0)
@@ -10,6 +21,7 @@ cap.set(10,150)
 
 while True:
     success , img = cap.read()
+    findColor(img,myColors)
     cv2.imshow("Webcam Video", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
