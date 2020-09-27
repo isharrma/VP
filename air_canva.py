e51cd1a9de08bfcf2a316c747c0b7ca3952323df
isharrma <54413011+isharrma@users.noreply.github.com>
2020-09-27 17:44:09 +0530

    Update air_canva.py

--- air_canva.py
+++ air_canva.py
@@ -4,6 +4,8 @@ import numpy as np
 def empty(a):
     pass
 
+kernel = np.ones((5,5),np.uint8)
+
 frameHeight = 640
 frameWidth = 480
 cap = cv2.VideoCapture(0)
@@ -34,11 +36,26 @@ def getContours(img):
 while True:
     success ,img = cap.read()
     imgCanny = cv2.Canny(img,200,200)
-    #imgflip = cv2.flip(img, 1)
+    imgflip = cv2.flip(img, 1)
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    h_min = cv2.getTrackbarPos("Hue Min","Trackbars")
+    s_min = cv2.getTrackbarPos("Sat Min","Trackbars")
+    v_min = cv2.getTrackbarPos("Val Min","Trackbars")
+    h_max = cv2.getTrackbarPos("Hue Max","Trackbars")
+    s_max = cv2.getTrackbarPos("Sat Min","Trackbars")
+    v_max = cv2.getTrackbarPos("Val Min","Trackbars")
+    lower = np.array([h_min,s_min,v_min])
+    upper = np.array([h_max,s_max,v_max])
+    
+    mask = cv2.inRange(imgHSV,lower,upper)
+    mask = cv2.erode(mask,kernel,iterations=1)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN,kernel)
+    mask = cv2.dilate(mask,kernel, iterations=1)
+
     getContours(imgCanny)
     cv2.imshow("HSV",imgHSV)
-    cv2.imshow("Canny",imgCanny)
-    cv2.imshow("Image",img)
+    cv2.imshow("Mask",mask)
+    cv2.imshow("Image",imgflip)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break 
